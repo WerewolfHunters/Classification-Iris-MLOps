4586989584e3dede83c3e2c47f4ddab51ef59f07
WerewolfHunters <awwab.mahimi.0074@gmail.com>
2025-06-13 00:34:44 +0530

    Fix: Removed submodule and added app folder as regular directory

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import numpy as np
+import joblib
+
+# Load Model
+model_path = r"../app/iris_model.pkl"
+model = joblib.load(model_path)
+
+app = FastAPI()
+
+class IrisInput(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the Iris Predictor API"}
+
+@app.post("/predict")
+def predict(data: IrisInput):
+    features = np.array([[data.sepal_length, data.sepal_width, data.petal_length, data.petal_width]])
+    pred = model.predict(features)[0]
+    return {"prediction": int(pred)}
